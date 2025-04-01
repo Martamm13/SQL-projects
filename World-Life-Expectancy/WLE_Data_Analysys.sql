133424a57b61fc7c19560b983440ec30d84703c0
Marta <mmmdrag@gmail.com>
2025-04-01 20:24:06 +0200

    committ

--- World-Life-Expectancy/WLE_Data_Analysys.sql
+++ World-Life-Expectancy/WLE_Data_Analysys.sql
@@ -1,29 +1,19 @@
--- World Life Expectancy Project (Exploratory Data Analysis)
--- The queries explore and analyze the world life expectancy dataset
--- Initial queries get the full dataset and check life expectancy trends
--- Further analysis looks at GDP and other factors affecting life expectancy
-
--- Select All Data
--- Purpose: Get all rows and columns from the world_life_expectancy table for a first look
+-- View all data
 SELECT * 
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- Life Expectancy Increase Over 15 Years
--- Purpose: Calculate the increase in life expectancy over 15 years for each country
+-- Life expectancy increase over 15 years
 SELECT Country, 
     MIN(`life expectancy`), 
     MAX(`life expectancy`),
     ROUND(MAX(`life expectancy`) - MIN(`life expectancy`),1) AS Life_Increase_15_Years
 FROM world_life_expectancy
-GROUP BY COUNTRY
+GROUP BY Country
 HAVING MIN(`life expectancy`) <> 0
 AND MAX(`life expectancy`) <> 0
-ORDER BY Life_Increase_15_Years DESC
-;
+ORDER BY Life_Increase_15_Years DESC;
 
--- Average Life Expectancy and GDP (Descending)
--- Purpose: Find the average life expectancy and GDP for each country, ordered by life expectancy from highest to lowest
+-- Avg life expectancy and GDP (high to low)
 SELECT Country,
     ROUND(AVG(`life expectancy`), 1) AS life_expectancy, 
     ROUND(AVG(GDP), 1) AS GDP
@@ -31,11 +21,9 @@ FROM world_life_expectancy
 GROUP BY Country
 HAVING life_expectancy > 0 
 AND GDP > 0
-ORDER BY life_expectancy DESC
-;
+ORDER BY life_expectancy DESC;
 
--- Average Life Expectancy and GDP (Ascending)
--- Purpose: Find the average life expectancy and GDP for each country, ordered by life expectancy from lowest to highest
+-- Avg life expectancy and GDP (low to high)
 SELECT Country,
     ROUND(AVG(`life expectancy`), 1) AS life_expectancy, 
     ROUND(AVG(GDP), 1) AS GDP
@@ -43,61 +31,46 @@ FROM world_life_expectancy
 GROUP BY Country
 HAVING life_expectancy > 0 
 AND GDP > 0
-ORDER BY life_expectancy ASC
-;
+ORDER BY life_expectancy ASC;
 
--- Order by GDP
--- Purpose: Get all data from the table and sort it by GDP
+-- Sort all data by GDP
 SELECT *
 FROM world_life_expectancy
-ORDER BY GDP
-;
+ORDER BY GDP;
 
--- High GDP Indicator
--- Purpose: Create a flag for countries with GDP >= 15000
-
+-- Flag high GDP countries
 SELECT 
     Country,
     CASE
         WHEN GDP >= 15000 THEN 1 
         ELSE 0
     END AS High_GDP_Count
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- High and Low GDP Life Expectancy Averages
--- Purpose: Calculate the number of countries and average life expectancy for high and low GDP groups
+-- Avg life expectancy for high/low GDP
 SELECT 
     SUM(CASE WHEN GDP >= 15000 THEN 1 ELSE NULL END) AS High_GDP_Count,
     ROUND(AVG(CASE WHEN GDP >= 15000 THEN `life expectancy` END), 1) AS Avg_High_GDP_Life_Expectancy,
 	SUM(CASE WHEN GDP <= 15000 THEN 1 ELSE NULL END) AS Low_GDP_Count,
     ROUND(AVG(CASE WHEN GDP <= 15000 THEN `life expectancy` END), 1) AS Avg_Low_GDP_Life_Expectancy
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- Select All Data
--- Purpose: Get all rows and columns from the world_life_expectancy table
+-- View all data
 SELECT *
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- Status and Life Expectancy
--- Purpose: Count distinct countries and calculate the average life expectancy by status
+-- Avg life expectancy by status
 SELECT Status,
 	COUNT(DISTINCT Country),
 	ROUND(AVG(`life expectancy`),1)
 FROM world_life_expectancy
-GROUP by Status
-;
+GROUP BY Status;
 
--- Select All Data
--- Purpose: Get all rows and columns from the world_life_expectancy table
+-- View all data
 SELECT *
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- Life Expectancy and BMI
--- Purpose: Calculate the average life expectancy and BMI for each country, sorted by life expectancy
+-- Avg life expectancy and BMI
 SELECT Country,
     ROUND(AVG(`life expectancy`), 1) AS life_expectancy, 
     ROUND(AVG(BMI), 1) AS BMI
@@ -105,17 +78,13 @@ FROM world_life_expectancy
 GROUP BY Country
 HAVING ROUND(AVG(`life expectancy`), 1) > 1 
 AND ROUND(AVG(BMI), 1) > 1
-ORDER BY life_expectancy DESC
-;
+ORDER BY life_expectancy DESC;
 
--- Select All Data
--- Purpose: Get all rows and columns from the world_life_expectancy table
+-- View all data
 SELECT *
-FROM world_life_expectancy
-;
+FROM world_life_expectancy;
 
--- Rolling Total of Adult Mortality
--- Purpose: Calculate the rolling total of adult mortality for countries with 'United' in their name
+-- Rolling adult mortality for 'United' countries
 SELECT Country,
        Year,
        `life expectancy`,
@@ -125,5 +94,4 @@ SELECT Country,
         WHERE w2.Country = w1.Country
           AND w2.Year <= w1.Year) AS Rolling_Total
 FROM world_life_expectancy AS w1
-WHERE Country LIKE '%United%'
-;
+WHERE Country LIKE '%United%';
